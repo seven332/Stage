a6e4ee1a1e057dc14eee2e5efca30020b73a1bd2
seven332 <seven332@163.com>
2017-06-04 15:13:32 +0800

    Fix DialogScene

--- demo/src/main/java/com/hippo/stage/demo/scene/DialogScene.java
+++ demo/src/main/java/com/hippo/stage/demo/scene/DialogScene.java
@@ -23,6 +23,7 @@ package com.hippo.stage.demo.scene;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
+import android.util.TypedValue;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -38,7 +39,10 @@ public abstract class DialogScene extends com.hippo.stage.dialog.DialogScene {
   @Override
   protected void onCreate(@Nullable Bundle args) {
     super.onCreate(args);
-    setThemeAttrId(R.attr.alertDialogTheme);
+
+    TypedValue outValue = new TypedValue();
+    getActivity().getTheme().resolveAttribute(R.attr.alertDialogTheme, outValue, true);
+    setTheme(outValue.resourceId);
   }
 
   @Nullable
